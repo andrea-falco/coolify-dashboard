8cf763228a9e49c12b38faa534471cebb66d07e2
Andrea Falco <>
2025-10-24 16:28:59 +0200

    Initial commit

---
+++ backend/src/main/java/xyz/andreafalco/coolify/dashboard/CoolifyDashboardBeApplication.java
@@ -0,0 +1,13 @@
+package xyz.andreafalco.coolify.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolifyDashboardBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolifyDashboardBeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/xyz/andreafalco/coolify/dashboard/CoolifyDashboardBeApplicationTests.java
@@ -0,0 +1,15 @@
+package xyz.andreafalco.coolify.dashboard;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolifyDashboardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+        Assertions.assertTrue(true);
+	}
+
+}
